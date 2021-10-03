d3406bd5079423a31f00620d956635760ab86fc9
Amal Ragh K <78066884+AmalRaghk@users.noreply.github.com>
2021-10-03 15:20:35 +0530

    Update operations.py

--- operations.py
+++ operations.py
@@ -12,6 +12,6 @@ def operations(left: int,middle: str, right: int):
 #gets input rom the user             
 first=int(input("enter first numner:"))
 second=int(input("enter second numner:"))
-operation=str(input("enter operatio:"))
+operation=str(input("enter operation:"))
 #calls the operation function
 operations(first ,operation ,second)
