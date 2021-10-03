b64b77c1e325a2506340203505173271b7850c89
Amal Ragh K <78066884+AmalRaghk@users.noreply.github.com>
2021-10-03 15:00:10 +0530

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,17 @@
+#function to do operations
+def operations(left: int,middle: str, right: int):
+    #checks for the operations
+    if middle=='+':
+        print(left + right)
+    if middle=='-':
+        print(left - right)
+    if middle=='/':
+        print(left / right)
+    if middle=='*':
+        print(left * right)
+#gets input rom the user             
+first=int(input("enter first numner:"))
+second=int(input("enter second numner:"))
+operation=str(input("enter operatio:"))
+#calls the operation function
+operations(first ,operation ,second)
