fd7d32763ac6b01784bc8d7182e0ad76c613921d
Sharun E Rajeev <54183649+sharunrajeev@users.noreply.github.com>
2021-10-15 23:22:51 +0530

    Create print.py

---
+++ print.py
@@ -0,0 +1,12 @@
+# Python program to print a data
+print("Data") # Displays -> Data
+
+x = "Data"
+print(x) # Displays -> Data
+
+print("Hi",end=".") # Ends with . instead of next line character
+
+print("Hi")
+print("People") 
+# Displays -> Hi
+#             People              
